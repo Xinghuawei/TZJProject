069e713c55c2c61a949876f30237e2dc8cdcb197
Xinghuawei <wxh52794@gmail.com>
2019-06-05 15:59:26 -0700

    images deleted and added to cloud

--- miniprogram/pages/company/company.js
+++ miniprogram/pages/company/company.js
@@ -12,47 +12,47 @@ Page({
     var data = {
       "datas": [{
           "id": 1,
-        "imgurl": "../../images/reward1.jpg"
+        "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward1.jpg?sign=4270f15d9822c50b89b61695f1961e65&t=1559775121"
         },
         {
           "id": 2,
-          "imgurl": "../../images/reward2.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward2.png?sign=fce1ba79a6cf9179597218135af34e0f&t=1559775134"
         },
         {
           "id": 3,
-          "imgurl": "../../images/reward3.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward3.png?sign=9632d0285fe525172f159bcc7f784f69&t=1559775144"
         },
         {
           "id": 4,
-          "imgurl": "../../images/reward4.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward4.png?sign=d51a9c6665c53b80ddbf159ed261fb26&t=1559775157"
         },
         {
           "id": 5,
-          "imgurl": "../../images/reward5.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward5.png?sign=48ef6e7d4ce1e60093e0c913d64da959&t=1559775169"
         },
         {
           "id": 6,
-          "imgurl": "../../images/reward6.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward6.png?sign=8f17a9fbb460232133ee5236afcbb448&t=1559775179"
         },
         {
           "id": 7,
-          "imgurl": "../../images/reward7.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward7.png?sign=a846e3c4d3225ad8dd6aa1aa4031b7cb&t=1559775195"
         },
         {
           "id": 8,
-          "imgurl": "../../images/reward8.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward8.png?sign=01b74cabf970104b3f8a1a4d16200232&t=1559775210"
         },
         {
           "id": 9,
-          "imgurl": "../../images/reward9.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward9.png?sign=7b1b9647750afd8b7c7609ff1a4686cc&t=1559775219"
         },
         {
           "id": 10,
-          "imgurl": "../../images/reward10.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/ward10.png?sign=622e2acfc7bde042027b34024831cc77&t=1559775228"
         },
         {
           "id": 11,
-          "imgurl": "../../images/reward11.png"
+          "imgurl": "https://747a-tzj-wechat-project-44v3a-1259195337.tcb.qcloud.la/reward11.png?sign=0b06ab0ce8fa0f8d489a1c0609045b46&t=1559775237"
         }
       ]
     };
