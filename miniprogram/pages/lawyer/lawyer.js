753a7f6c05a5da8fcae76b3db91c8b8bdc007318
heykenwu <yanhwan@gmail.com>
2019-06-08 00:24:58 -0700

    ddd
    gg

--- miniprogram/pages/lawyer/lawyer.js
+++ miniprogram/pages/lawyer/lawyer.js
@@ -1,2 +1,25 @@
 
+Page({
+  data: {
+    hi:"hi"
+  },
+  onLoad: function () {
+    var that = this
+    const db = wx.cloud.database();
+    db.collection('lawyers').get({
+      success: res => {
+        that.setData({
+          lawyers: res.data
+        });
+      },
+    })
+    console.log(this.data)
+  },
+  lawyerInfo: function(event){
+    let id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '../lawyerInfo/lawyerInfo?id='+id
+    })
+  },
+})
   
---
+++ miniprogram/pages/lawyerInfo/lawyerInfo.js
@@ -0,0 +1,76 @@
+// miniprogram/pages/lawyerInfo/lawyerInfo.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    var id = options.id;
+    var that = this;
+    const db = wx.cloud.database();
+    db.collection('lawyers').where({ _id: id }).get({
+      success: res => {
+        that.setData({
+          lawyer: res.data[0]
+        });
+      },
+    })
+    console.log(this.data);
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
